3b09788aa4624acd23bededa17d3a78542b0dce3
Naley420 <naley420.910@gmail.com>
2023-04-16 12:05:15 +0700

    finish project

--- fake_instagram/core/views.py
+++ fake_instagram/core/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 from .models import Profile, Post, LikePost, FollowersCount
+from itertools import chain
 
 # Create your views here.
 @login_required(login_url='signin')
@@ -12,8 +13,22 @@ def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
     
-    posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
+    user_following_list = []
+    feed = []
+    
+    user_following = FollowersCount.objects.filter(follower=request.user.username)
+    
+    for user in user_following:
+        user_following_list.append(user.user)
+        
+    for username in user_following_list:
+        feed_list = Post.objects.filter(user=username)
+        feed.append(feed_list)
+        
+    feed_list = list(chain(*feed))
+        
+    
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list,})
 
 def signup(request):
     
@@ -173,3 +188,25 @@ def follow(request):
             return redirect('/profile/' + user)
     else:
         return redirect('/')
+    
+@login_required(login_url='signin') 
+def search(request):
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        username_object = User.objects.filter(username__icontains=username)
+        
+        username_profile = []
+        username_profile_list = []
+        
+        for user in username_object:
+            username_profile.append(user.id)
+            
+        for id in username_profile:
+            profile_list = Profile.objects.filter(id_user=id)
+            username_profile_list.append(profile_list)
+
+        username_profile_list = list(chain(*username_profile_list))
+    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
